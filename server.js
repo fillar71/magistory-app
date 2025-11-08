d7620d2941ac015e3464a596ae995f1d861859b2
Filla Ramadhan <fillar71@gmail.com>
2025-11-09 00:05:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { generateScript } from "./controllers/geminiController.js";
-import { renderVideo } from "./controllers/renderController.js";
 
 dotenv.config();
 const app = express();
@@ -12,15 +11,15 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://magistory-frontend.vercel.app",
+      "https://magistory-frontend.vercel.app"
     ],
     methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
   })
 );
 
 app.get("/", (req, res) => res.send("✅ Magistory Backend aktif!"));
 app.post("/api/generate-script", generateScript);
-app.post("/api/render", renderVideo);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`✅ Backend berjalan di port ${PORT}`));
